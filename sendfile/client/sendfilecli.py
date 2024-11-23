15892377831fe38977ce1753aea6e7244234fafc
ZootZoot-25 <49415426+ZootZoot-25@users.noreply.github.com>
2024-11-22 23:24:06 -0800

    first commit

---
+++ sendfile/client/sendfilecli.py
@@ -0,0 +1,30 @@
+# *****************************************************
+# This file implements a client for sending and receiving
+# files to/from the sendfileserv.py server. The client
+# can upload, download, and list files on the server using cmds.py
+# *****************************************************
+import socket
+import sys
+from sendfile.client.cmds import execute_command
+
+# Validate command-line arguments
+if len(sys.argv) != 3:
+    print("Usage: python sendfilecli.py <server machine> <server port>")
+    sys.exit(1)
+
+# Set server details
+server_name = sys.argv[1]
+control_port = int(sys.argv[2])
+
+# Connect to the server's control channel
+control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+control_socket.connect((server_name, control_port))
+print(f"Connected to the FTP server at {server_name} on port {control_port}")
+
+# Start command loop
+while True:
+    command = input("ftp> ").strip()
+    if execute_command(control_socket, server_name, command) == "quit":
+        break
+
+print("Disconnected from the server.")
---
+++ sendfile/server/sendfileserv.py
@@ -0,0 +1,121 @@
+
+# *****************************************************
+# This file implements a server for receiving the file
+# sent using sendfile(). The server receives a file and
+# prints it's contents.
+# *****************************************************
+
+import socket
+import os
+from sendfile.server.ephemeral import get_ephemeral_port
+
+# Control channel port
+controlPort = 1234
+# Create and bind control socket
+controlSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+controlSocket.bind( ( '', controlPort ) )
+controlSocket.listen(1)    # Listen for client connections
+
+print( f"FTP server is listening on port {controlPort}" )
+
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll( sock, numBytes ):
+	# Receive all bytes from a socket
+	recvBuff = ""
+	tmpBuff = ""
+	
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff
+	
+	return recvBuff
+		
+# Accept connections forever
+while True:
+	print( "Waiting for connections..." )
+	clientSocket, addr = controlSocket.accept()   # Accept connections
+	print( "Accepted connection from client: ", addr )
+	print( "\n" )
+
+	while True:
+		command = clientSocket.recv( 1024 ).decode().strip()    # Receive FTP command
+		if not command:
+			break   # Cleint disconnected
+		
+		print( f"Received command: { command }" )
+		
+		if command == 'ls':
+			# List all files in the directory
+			files = '\n'.join( os.listdir( '.' ) )
+			clientSocket.send( files.encode() )
+			
+		elif command.startswith( "get" ):
+			filename = command.split()[1] # extract filename
+			if os.path.exists( filename ):
+				# Generate ephemeral port for the data channel
+				dataPort = get_ephemeral_port()
+				clientSocket.send( str( dataPort ).encode() )	# Send port to client
+				
+				# Open data channel
+				dataSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+				dataSocket.bind( ( '', dataPort ) )
+				dataSocket.listen(1)
+				dataConn, _ = dataSocket.accept()
+				
+				# Send file over data channel
+				with open( filename, 'rb' ) as f:
+					dataConn.sendfile(f)
+					
+				dataConn.close()    # Close data connection
+				dataSocket.close()
+				print( f"Sent { filename } to client" )
+			else:
+				clientSocket.send( b"ERROR: File not found" )
+		
+		elif command.startswith( 'put' ):
+			filename = command.split()[1]	# Extract filename
+			
+			# Generate ephemeral port for receiving data
+			dataPort = get_ephemeral_port()
+			clientSocket.send( str( dataPort ).encode() )	# Send port to clinet
+
+			# Open data channel
+			dataSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+			dataSocket.bind( ( '', dataPort ) )
+			dataSocket.listen(1)
+			dataConn, _ = dataSocket.accept()
+
+			# Receive file over data channel
+			with open( filename, 'wb' ) as f:
+				while True:
+					data = dataConn.recv( 1024 )
+					if not data:
+						break
+					f.write( data )
+			
+			dataConn.close()
+			dataSocket.close()
+			print( f"Received { filename } from client" )
+
+		elif command == "quit":
+			print( "Closeing connection with client" )
+			clientSocket.close()
+			break
+
+	print( "Waiting for new connection" )
+	
